dcae0414ccd13b38b838a4378536166d39723631
srija2004ghorai <142221215+srija2004ghorai@users.noreply.github.com>
2023-08-15 00:24:31 +0530

    Add files via upload

---
+++ ItsCoffeeOclock.js
@@ -0,0 +1,12 @@
+function check(){
+    var a = document.getElementById("Username").value;
+    var b = document.getElementById("Password").value;
+    var d = "srija333@";
+    var e = "123456";
+    if(a == d){ 
+        if(b == e){
+   open("index.html");
+    }
+}
+
+}
